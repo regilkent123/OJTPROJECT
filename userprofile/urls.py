fe9fa8649f568621f8cb65313a722b7f823e6660
iamkatrinaanne <ktredes@gmail.com>
2018-05-04 10:03:59 +0800

    added html files for login and design of log in

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,7 +1,12 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.LoginView.as_view(), name='login'),
-	path('register/', views.RegisterView.as_view(), name='register')
+    path('', views.LoginView.as_view(), name='login'),
+    path('afterlogin/', views.AfterLoginView.as_view(), name='afterlogin'),
+    path('invalidlogin/', views.InvalidLoginView.as_view(), name='invalidlogin'),
+    path('logout/', views.LogOutView.as_view(), name='logout'),
+    path('register/', views.RegisterView.as_view(), name='register')
+
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from django.views import generic
 
 class RegisterView(generic.TemplateView):
-	template_name = 'userprofile/register.html'
+    template_name = 'userprofile/register.html'
 
 class LoginView(generic.TemplateView):
-	template_name = 'userprofile/login.html'
+    template_name = 'userprofile/login.html'
+
+class AfterLoginView(generic.TemplateView):
+    template_name = 'userprofile/loggedin.html'
+
+class InvalidLoginView(generic.TemplateView):
+    template_name = 'userprofile/invalid_login.html'
+
+class LogOutView(generic.TemplateView):
+    template_name = 'userprofile/logout.html'
+
+
 # Create your views here.
