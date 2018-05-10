7e448f9cc2e5a4e091f88197a073b866df412aeb
Regil Sembrino <regilsembrino@gmail.com>
2018-05-10 13:54:53 +0800

    working login module

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('afterlogin/', views.AfterLoginView.as_view(), name='afterlogin'),
     path('invalidlogin/', views.InvalidLoginView.as_view(), name='invalidlogin'),
     path('logout/', views.LogOutView.as_view(), name='logout'),
-    path('register/', views.RegisterView.as_view(), name='register')
-
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('loggedin/', views.auth_login, name='loggedin'),
+    path('registers/', views.register, name='registers')
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,12 +1,42 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.models import User
 
 class RegisterView(generic.TemplateView):
     template_name = 'userprofile/register.html'
 
+def register(request):
+    usernames = request.POST['username']
+    password = request.POST['password']
+    firstname = request.POST['first_name']
+    lastname = request.POST['last_name']
+    email = request.POST['email']
+    u=User(username=usernames, first_name=firstname, last_name=lastname, email=email)
+    u.set_password(password)
+    u.save()
+
+    return HttpResponse('success')
+
 class LoginView(generic.TemplateView):
     template_name = 'userprofile/login.html'
 
+
+def auth_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        auth_user = authenticate(username=username, password=password)
+
+        if auth_user is not None:
+            login(request, auth_user)
+            return HttpResponseRedirect('/afterlogin/')
+        else :
+            return HttpResponse('error')
+    return HttpResponse('not allowed') 
 
 class AfterLoginView(generic.TemplateView):
     template_name = 'userprofile/loggedin.html'
