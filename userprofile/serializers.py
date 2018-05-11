479d424e7cf9e9db22d78e763e8e749110e7aec5
Regil Sembrino <regilsembrino@gmail.com>
2018-05-11 10:42:18 +0800

    change the fields in serializers.py and change relative paths to static paths

--- userprofile/serializers.py
+++ userprofile/serializers.py
@@ -13,4 +13,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username','first_name','last_name', 'detail_url')
