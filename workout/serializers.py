27454a904ac557326ea96a2831d5b7c193cd5de7
Regil Sembrino <regilsembrino@gmail.com>
2018-05-21 13:42:09 +0800

    workout_type will now give its true value

--- workout/serializers.py
+++ workout/serializers.py
@@ -2,8 +2,11 @@ from rest_framework import serializers
 from .models import Workout
 
 class WorkoutSerializer(serializers.ModelSerializer):
-    detial_url = serializers.CharField(read_only=True)
+    detail_url = serializers.CharField(read_only=True)
+    workout_type = serializers.SerializerMethodField()
 
     class Meta:
         model = Workout
         fields = '__all__'
+
+    workout_type = serializers.CharField(source = 'get_workout_type_display')
